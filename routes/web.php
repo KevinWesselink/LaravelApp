6b522d828348d19aded26a9adc618dca169ea52f
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-08 14:32:45 +0100

    31.19min
    Route geüpdatet, welcome.blade.php hernoemd en inhoud aangepast.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quarat consequatur! Expedita ab consecetur tenetur delensiti'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quarat consequatur! Expedita ab consecetur tenetur delensiti'
+            ]
+        ]
+    ]);
 });
