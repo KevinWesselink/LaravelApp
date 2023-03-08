a56f4e2386b9c0a83ecb68f6d9de520d9dc0c2f0
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-08 13:58:04 +0100

    Update web.php
    Use en meerdere routes weggehaald.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,18 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/hello', function () {
-    return response("<h1>Hello World</h1>", 200)
-        -> header('Content-Type', 'text/plain')
-        -> header('foo', 'bar');
-});
-
-Route::get('/posts/{id}', function($id) {
-    return response('Post ' . $id);
-}) ->where('id', '[0-9]+');
-
-Route::get('/search', function(Request $request){
-    return request->name . ' ' . request->city;
 });
