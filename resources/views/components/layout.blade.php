ec0a6471f13b672eef4a28a02219cde344ed400e
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-23 09:25:41 +0100

    Auth Links
    Bepaalde links tonen zich nu alleen als gebruikers zijn ingelogd.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,6 +30,20 @@
         <a href="/">
             <img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" />
         </a>
+        @auth
+        <ul class="flex space-x-6 mr-6 text-lg">
+            <li>
+                <span class="font-bold uppercase">
+                    Welcome {{auth()->user()->name}}
+                </span>
+            </li>
+            <li>
+                <a href="/listings/manage" class="hover:text-laravel">
+                    <i class="fa-solid fa-gear"></i> Manage Listings
+                </a>
+            </li>
+        </ul>
+        @else
         <ul class="flex space-x-6 mr-6 text-lg">
             <li>
                 <a href="/register" class="hover:text-laravel">
@@ -41,6 +55,7 @@
                     <i class="fa-solid fa-arrow-right-to-bracket"></i> Login
                 </a>
             </li>
+            @endauth
         </ul>
     </nav>
     <main>
